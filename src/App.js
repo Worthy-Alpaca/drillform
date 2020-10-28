c609b49a10c1c3bcca48a766d77a983cc83a8884
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-10-28 14:00:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+const { hooks } = require('./webhooks.json');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    ircnick: "",
+    timezone: "",
+    jumprange: "",
+    homebase: "",
+  }
+
+  _handleChange = (e) => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      }
+    )
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.ircnick === "" || this.state.timezone === "" || this.state.jumprange === "") {
+      const input1 = document.getElementById("input1")
+      const input2 = document.getElementById('input2');
+      const input3 = document.getElementById('input3');
+      input1.classList.add('border_red');
+      input2.classList.add('border_red');
+      return input3.classList.add('border_red');
+    }
+    var timezone;
+    var jumprange;
+    var homebase;
+    if (this.state.timezone.toLowerCase().includes("utc")) {
+      timezone = this.state.timezone.toLowerCase().replace("utc", "");
+    } else {
+      timezone = this.state.timezone
+    }
+
+    if (this.state.jumprange.toLowerCase().includes("ly")) {
+      jumprange = this.state.jumprange.toLowerCase().replace("ly", "");
+    } else {
+      jumprange = this.state.jumprange;
+    }
+
+    if (this.state.homebase === "") {
+      homebase = "No homebase";
+    } else {
+      homebase = this.state.homebase;
+    }
+
+    var params = {
+      username: "Drill Request",
+      avatar_url: "",
+      embeds: [
+        {          
+          "title": "New drill request",
+          "color": 15258703,
+          "fields": [
+            {
+              "name": "IRC nick",
+              "value": this.state.ircnick,
+              "inline": true
+            },
+            {
+              "name": "Timezone",
+              "value": `UTC ${timezone}`,
+              "inline": true
+            },
+            {
+              "name": "Jumprange of ratship",
+              "value": `${jumprange}ly`,
+            },
+            {
+              "name": "Homebase",
+              "value": homebase
+            }
+          ]
+        }
+      ]
+    }
+    
+    hooks.forEach(element => {
+      fetch(element, {
+        method: "POST",
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(params)
+      }).then(res => {
+        if (res.status === 204) {
+          const form = document.getElementById("form");
+          const message = document.getElementById('success_message');
+          form.style.display = 'none';
+          message.style.display = 'block';
+        } else {
+          const form = document.getElementById("form");
+          const message = document.getElementById('failure_message');
+          form.style.display = 'none';
+          message.style.display = 'block';
+        }
+      })
+        .catch(err => {
+          console.log("err")
+        })
+    })
+  }
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <form className="modal-content" id='form'>
+          <div className="container">
+
+            <label htmlFor="ircnick"><b style={{ color: "white" }}>IRC Nick *</b></label>
+            <input id="input1" type="text" placeholder="Enter Your IRC Nick" name="ircnick" onChange={this._handleChange} required />
+
+            <label htmlFor="timezone"><b style={{ color: "white" }}>Timezone in UTC *</b></label>
+            <input id="input2" type="text" placeholder="Enter Timezone" name="timezone" onChange={this._handleChange} required />
+
+            <label htmlFor="jumprange"><b style={{ color: "white" }}>The jumprange of your ratship in lightyears (ly) *</b></label>
+            <input id="input3" type="text" placeholder="Enter Jumprange" name="jumprange" onChange={this._handleChange} required />
+
+            {/* <p style={{ color: "white" }}>If you have a homesystem you can enter it here</p> */}
+            <label htmlFor="homebase"><b style={{ color: "white" }}>Your Homesystem</b></label>
+            <input id="input4" type="text" placeholder="Enter Homebase" name="homebase" onChange={this._handleChange} />
+
+            <button onClick={this.handleFormSubmit}>Submit</button>
+            <p style={{ color: "white" }}><i>* is required</i></p>
+
+          </div>
+        </form>
+        <h2 id='success_message' style={{ textAlign: 'center', display: "none", color: "white" }}>Thank you for using this system. Your Overseer will contact you regarding a drill.</h2>
+        <h2 id='failure_message' style={{ textAlign: 'center', display: "none", color: "red" }}>Something went wrong. Please reload the page and make sure to enter all required information!</h2>
+      </React.Fragment>
+    )
+  }
+
 }
 
 export default App;
